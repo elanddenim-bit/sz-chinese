a98ccc71fadbb78cbf40d1d567ec38f1f9b26af1
elanddenim-bit <elanddenim@gmail.com>
2026-09-02 12:14:08 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const C='szcn-beta-1';
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'])));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin)return;
+  e.respondWith(
+    fetch(e.request).then(r=>{const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp));return r;})
+    .catch(()=>caches.match(e.request))
+  );
+});
